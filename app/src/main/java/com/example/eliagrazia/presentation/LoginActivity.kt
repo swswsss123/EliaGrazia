ea5c48de73e4b316a8443bac742f601ee2bf6dd9
swswsss123 <40319512+swswsss123@users.noreply.github.com>
2022-06-03 17:55:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/eliagrazia/presentation/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.eliagrazia.presentation
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.eliagrazia.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
